22e840858c2ffdce1cb56dcb99267965df0f79e8
mur3l <tippitauno@gmail.com>
2025-09-08 17:00:48 +0300

    ShopTARge24_Project REPAIR COMMIT

---
+++ ShopTARge24/ShopTARge24/Controllers/SpaceshipsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge24.Data;
+using ShopTARge24.Models.Spaceships;
+
+namespace ShopTARge24.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        private readonly ShopTARge24Context _context;
+
+        public SpaceshipsController
+            (
+                ShopTARge24Context context
+            )
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Classification = x.Classification,
+                    BuiltDate = x.BuiltDate
+                });
+
+            return View(result);
+        }
+    }
+}
---
+++ ShopTARge24/ShopTARge24/Models/Spaceships/SpaceshipIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopTARge24.Models.Spaceships
+{
+    public class SpaceshipIndexViewModel
+    {
+        public Guid? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Classification { get; set; }
+        public DateTime? BuiltDate { get; set; }
+        public int? Crew {  get; set; }
+        public int? EnginePower { get; set; }
+    }
+}
