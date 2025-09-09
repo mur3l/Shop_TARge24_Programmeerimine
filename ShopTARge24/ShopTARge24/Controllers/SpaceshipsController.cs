605ae3561730c9c32f9f05667067db563bfbae29
mur3l <tippitauno@gmail.com>
2025-09-09 10:29:57 +0300

    End of Class COMMIT

--- ShopTARge24/ShopTARge24/Controllers/SpaceshipsController.cs
+++ ShopTARge24/ShopTARge24/Controllers/SpaceshipsController.cs
@@ -31,5 +31,13 @@ namespace ShopTARge24.Controllers
 
             return View(result);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            SpaceshipCreateViewModel result = new();
+
+            return View("Create", result);
+        }
     }
 }
