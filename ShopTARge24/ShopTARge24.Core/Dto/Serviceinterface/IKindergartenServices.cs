0353c8f7ae3f648fdd9c9957cd727cd3d798281e
mur3l <tippitauno@gmail.com>
2025-09-16 13:23:05 +0300

    End of Class COMMIT (BROKEN)

---
+++ ShopTARge24/ShopTARge24.Core/Dto/Serviceinterface/IKindergartenServices.cs
@@ -0,0 +1,15 @@
+﻿using ShopTARge24.Core.Domain;
+using ShopTARge24.Core.Dto;
+
+
+namespace ShopTARge24.Core.ServiceInterface
+{
+    public interface IKindergartenServices
+    {
+        Task<Kindergarten> Create(KindergartenDto dto);
+        Task<Kindergarten> DetailAsync(Guid id);
+        Task<Kindergarten> Delete(Guid id);
+
+        Task<Kindergarten> Update(KindergartenDto dto);
+    }
+}
