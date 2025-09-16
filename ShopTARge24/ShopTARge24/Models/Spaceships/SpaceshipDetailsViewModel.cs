79702e9165ff4e9bc96f2675e0b116962458e19f
mur3l <tippitauno@gmail.com>
2025-09-16 10:54:35 +0300

    CreateUpdate, Index Changes & DetailsView COMMIT

---
+++ ShopTARge24/ShopTARge24/Models/Spaceships/SpaceshipDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopTARge24.Models.Spaceships
+{
+    public class SpaceshipDetailsViewModel
+    {
+        public Guid? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Classification { get; set; }
+        public DateTime? BuiltDate { get; set; }
+        public int? Crew { get; set; }
+        public int? EnginePower { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
