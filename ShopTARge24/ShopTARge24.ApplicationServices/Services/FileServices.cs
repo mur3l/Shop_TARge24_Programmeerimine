0f37529bf9b3cd7e40a091455777fae04bd68766
mur3l <tippitauno@gmail.com>
2025-09-22 18:10:46 +0300

    FileToApi & Hero COMMIT

---
+++ ShopTARge24/ShopTARge24.ApplicationServices/Services/FileServices.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Hosting;
+using ShopTARge24.Core.Domain;
+using ShopTARge24.Core.Dto;
+using ShopTARge24.Core.Dto.Serviceinterface;
+using ShopTARge24.Data;
+
+namespace ShopTARge24.ApplicationServices.Services
+{
+    public class FileServices : IFileServices
+    {
+        private readonly IHostEnvironment _webHost;
+        private readonly ShopTARge24Context _context;
+        public FileServices
+            (
+                IHostEnvironment webHost,
+                ShopTARge24Context context
+            )
+        { 
+            _webHost = webHost;
+        }
+        public void FilesToApi(SpaceshipDto dto, Spaceships domain)
+        {
+            if (dto.Files != null && dto.Files.Count > 0)
+            {
+                if (!Directory.Exists(_webHost.ContentRootPath + "\\multipleFileUpload\\"))
+                {
+                    Directory.CreateDirectory(_webHost.ContentRootPath + "\\multipleFileUpload\\");
+                }
+            }
+
+            foreach (var file in dto.Files)
+            {
+                string uploadsFolder = Path.Combine(_webHost.ContentRootPath, "multipleFileUpload");
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+
+                    FileToApi path = new FileToApi
+                    {
+                        Id = Guid.NewGuid(),
+                        ExistingFilePath = uniqueFileName,
+                        SpaceshipId = domain.Id
+                    };
+
+                    _context.FileToApis.AddAsync(path);
+                }
+            }
+
+        }
+    }
+}
