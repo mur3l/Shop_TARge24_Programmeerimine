0b0a81a3e3f7039c3c012848f6c90ba219794aa1
mur3l <tippitauno@gmail.com>
2025-09-29 19:44:49 +0300

    RealEstateDomain COMMIT

--- ShopTARge24/ShopTARge24.Core/Domain/RealEstate.cs
+++ ShopTARge24/ShopTARge24.Core/Domain/RealEstate.cs
@@ -8,5 +8,12 @@ namespace ShopTARge24.Core.Domain
 {
     public class RealEstate
     {
+        public Guid Id { get; set; }
+        public double? Area { get; set; }
+        public string? Location { get; set; }
+        public int? RoomNumber {  get; set; }
+        public string? BuildingType { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
     }
 }
