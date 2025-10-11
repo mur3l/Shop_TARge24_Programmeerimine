45447edbf1bc14e70efacc1ee46d25296b7e8778
mur3l <tippitauno@gmail.com>
2025-10-11 17:14:29 +0300

    View.cshtml ADD File picker_FileServices KJindergarten FIX_COMMIT

---
+++ ShopTARge24/ShopTARge24.Data/Migrations/20251011135556_AddKindergartenIdToFileToApi.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopTARge24.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddKindergartenIdToFileToApi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KindergartenId",
+                table: "FileToApis",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KindergartenId",
+                table: "FileToApis");
+        }
+    }
+}
--- ShopTARge24/ShopTARge24/Models/Kindergarten/KindergartenCreateUpdateViewModel.cs
+++ ShopTARge24/ShopTARge24/Models/Kindergarten/KindergartenCreateUpdateViewModel.cs
@@ -8,15 +8,15 @@ namespace ShopTARge24.Models.Kindergarten
     public class KindergartenCreateUpdateViewModel
     {
         public Guid? Id { get; set; }
-        public string Nr { get; set; }
-        public string GroupName { get; set; }
+        public string? Nr { get; set; }
+        public string? GroupName { get; set; }
         public int ChildrenCount { get; set; }
-        public string KindergartenName { get; set; }
-        public string TeacherName { get; set; }
+        public string? KindergartenName { get; set; }
+        public string? TeacherName { get; set; }
         public DateTime? CreatedAt { get; internal set; }
         public DateTime? ModifiedAt { get; internal set; }
 
-        public IList<IFormFile> Files { get; set; }
+        public IList<IFormFile>? Files { get; set; }
         public List<ImageViewModel> Images { get; set; } = new();
     }
 }
