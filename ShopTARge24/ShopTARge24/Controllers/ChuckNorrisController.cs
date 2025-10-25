b4bc9c5f140cfe4e2e182e4295786a11a144803e
mur3l <tippitauno@gmail.com>
2025-10-25 07:51:59 +0300

    Chuck Norris_Layout_Index_Model and Controller_COMMIT

---
+++ ShopTARge24/ShopTARge24/Controllers/ChuckNorrisController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge24.Models.ChuckNorris;
+using System.Net;
+using System.Text.Json;
+
+namespace ShopTARge24.Controllers
+{
+    public class ChuckNorrisController : Controller
+    {
+        public IActionResult Index()
+        {
+            ChuckNorrisModel Joke = GetRandomJoke();
+            return View(Joke);
+        }
+
+        private ChuckNorrisModel GetRandomJoke()
+        {
+            string url = "https://api.chucknorris.io/jokes/random";
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                var joke = JsonSerializer.Deserialize<ChuckNorrisModel>(json, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                return joke;
+            }
+        }
+    }
+}
