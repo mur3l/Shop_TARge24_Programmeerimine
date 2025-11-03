20ed5c093b43b5e035555d2342ef7690f695a779
mur3l <tippitauno@gmail.com>
2025-11-03 14:17:32 +0200

    OpenWeather_Index COMMIT

--- ShopTARge24/ShopTARge24/Controllers/OpenWeatherController.cs
+++ ShopTARge24/ShopTARge24/Controllers/OpenWeatherController.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ShopTARge24.ApplicationServices;
+using ShopTARge24.ApplicationServices.Services;
 
 namespace ShopTARge24.Controllers
 {
     public class OpenWeatherController : Controller
     {
-        public IActionResult Index()
+        private readonly OpenWeatherService _openweatherService;
+        public OpenWeatherController()
         {
+            _openweatherService = new OpenWeatherService();
+        }
+
+        //Url päring
+        [HttpGet("/OpenWeather")]
+        public async Task<IActionResult> Index(string city = "Tallinn")
+        {
+            var weatherData = await _openweatherService.GetWeatherAsync(city);
+            ViewData["WeatherData"] = weatherData;
+            ViewData["City"] = city;
+
             return View();
         }
     }
