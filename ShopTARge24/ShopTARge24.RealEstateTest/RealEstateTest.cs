ca868faf76a9c8c77b3f748edb45a8fc639df200
mur3l <tippitauno@gmail.com>
2025-11-03 13:49:02 +0200

    RealEstateTest_LESSON and OpenWeather Branch_Service, Controller COMMIT

---
+++ ShopTARge24/ShopTARge24.RealEstateTest/RealEstateTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using ShopTARge24.Core.Dto;
+using ShopTARge24.Core.ServiceInterface;
+
+namespace ShopTARge24.RealEstateTest
+{
+    public class RealEstateTest : TestBase
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            //Arrange
+            RealEstateDto dto = new()
+            {
+                Area = 120.5,
+                Location = "Test Location",
+                RoomNumber = 3,
+                BuildingType = "Apartment",
+                CreatedAt = DateTime.UtcNow,
+                ModifiedAt = DateTime.UtcNow
+            };
+
+            //Act
+            var result = await Svc<IRealEstateServices>().Create(dto);
+
+            //Assert
+            Assert.NotNull(result);
+
+        }
+    }
+}
