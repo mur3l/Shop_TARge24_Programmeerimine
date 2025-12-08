25bb8beb063414a2aef4fe7d945c76972b1b31ca
mur3l <tippitauno@gmail.com>
2025-12-08 11:26:15 +0200

    Final CHAT Fix_TimeADD COMMIT

--- ShopTARge24/ShopTARge24/Hubs/ChatHub.cs
+++ ShopTARge24/ShopTARge24/Hubs/ChatHub.cs
@@ -4,9 +4,9 @@ namespace ShopTARge24.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string message, string time)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message, time);
         }
     }
 }
--- ShopTARge24/ShopTARge24/wwwroot/js/chat.js
+++ ShopTARge24/ShopTARge24/wwwroot/js/chat.js
@@ -4,9 +4,9 @@
 
 document.getElementById("sendButton").disabled = true;
 
-connectionChat.on("ReceiveMessage", (user, message) => {
+connectionChat.on("ReceiveMessage", (user, message, time) => {
     var msg = document.createElement("div");
-    msg.innerHTML = `<strong>${user}:</strong> ${message}`;
+    msg.innerHTML = `<strong>${user}</strong> (${time}): ${message}`;
     document.getElementById("messagesList").appendChild(msg);
 });
 
@@ -18,7 +18,9 @@ connectionChat.start().then(() => {
 document.getElementById("sendButton").addEventListener("click", () => {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
+    var time = new Date().toLocaleString();
 
-    connectionChat.send("SendMessage", user, message);
+    connectionChat.send("SendMessage", user, message, time);
+
     document.getElementById("messageInput").value = "";
 });
